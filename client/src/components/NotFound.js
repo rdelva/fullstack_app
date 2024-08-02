803b9624ac35f5443d1ac72c0d96162b68ac1210
rdelva <roseldelva@gmail.com>
2024-08-01 23:24:51 -0400

    changed text in Not Found and changed class attribute to className

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,9 @@
 const NotFound = () => {
     return (
         <main>
-            <div class="wrap">
-                <h2>Error</h2>
-                <p>Sorry! This Course no longer exists! Please return to the homepage.</p>
+            <div className="wrap">
+                <h2>404 - Not Found</h2>
+                <p>Sorry! The page you are looking for is not available or no longer exists.</p>
             </div>
         </main>
     );
