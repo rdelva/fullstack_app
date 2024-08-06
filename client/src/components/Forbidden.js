fa4369ef621061cd41036489d82ae5de2a0a9e3b
rdelva <roseldelva@gmail.com>
2024-08-06 01:41:45 -0400

    removed the signin link and changed the text

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,10 +1,10 @@
-import {Link } from 'react-router-dom';
+
 const Forbidden = () => {
     return (
         <main>
             <div className="wrap">
                 <h2>Forbidden</h2>
-                <p>Oh oh! You can't access this page! Please <Link to="/signin">sign in</Link> to have access.</p>
+                <p>Oh oh! You can't update this page!</p>
             </div>
         </main>
     );
