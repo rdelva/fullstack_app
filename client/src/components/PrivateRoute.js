4043a44d73baf6b7c91fc3cd4822b4b2b03fd251
rdelva <roseldelva@gmail.com>
2024-08-06 01:42:10 -0400

    added comment

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,6 +2,12 @@ import {useContext} from "react";
 import UserContext from "../context/UserContext";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
+
+
+/*This component control where the user goes if they try to access a part of the site is private.
+*if a user logs in they can go to CreateCourse and Update Couse if not they would have to go to the sign in page to get access
+*
+*/
 const PrivateRoute = () => {
 
     const {authUser } = useContext(UserContext);
