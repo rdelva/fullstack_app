4a7ba4cb1faa3ec4a7e081a7faaed74540dddd78
rdelva <roseldelva@gmail.com>
2024-08-10 19:24:19 -0400

    added 403 to the header

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -3,7 +3,7 @@ const Forbidden = () => {
     return (
         <main>
             <div className="wrap">
-                <h2>Forbidden</h2>
+                <h2>403 - Forbidden</h2>
                 <p>Oh oh! You can't update this page!</p>
             </div>
         </main>
