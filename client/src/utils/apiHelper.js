1dcebebe5cf93c47cb352c966a96c218c417fe99
rdelva <roseldelva@gmail.com>
2024-08-12 20:28:25 -0400

    update apiHelper

--- client/src/utils/apiHelper.js
+++ client/src/utils/apiHelper.js
@@ -14,8 +14,7 @@ export const api = (
     ) => {
 
     
-    const url = "https://timely-basbousa-10fd74.netlify.app/api" + path;
-    //old url for local const url = "http://localhost:5000/api" + path;
+    const url = "http://localhost:5000/api" + path;
    
 
     const options = {
